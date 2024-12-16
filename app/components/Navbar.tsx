89f90687db67fe67e12998c55bcf37079da7e973
karanssu <s0377009salemstate@gmail.com>
2024-12-16 13:08:57 -0500

    signin with google and logout finished

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,57 @@
+import { auth, signOut, signIn } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
 
-const Navbar = () => {
+const Navbar = async () => {
+	const session = await auth();
+
 	return (
 		<div className="px-5 py-3 bg-white shadow-sm font-work-sans">
 			<nav className="flex justify-between items-center">
 				<Link href="/">
 					<Image src="/logo.png" alt="logo" width={30} height={30} />
 				</Link>
+
+				<div className="flex items-center gap-5 text-black">
+					{session && session?.user ? (
+						<>
+							<Link
+								className="text-gray-600 font-medium hover:text-gray-800"
+								href="/startup/create"
+							>
+								<span>Create</span>
+							</Link>
+
+							<form
+								action={async () => {
+									"use server";
+
+									await signOut({ redirectTo: "/" });
+								}}
+							>
+								<button type="submit">Logout</button>
+							</form>
+
+							<Link
+								className="text-gray-600 font-medium hover:text-gray-800"
+								href={`/user/${session?.id}`}
+							>
+								<span>{session?.user?.name}</span>
+							</Link>
+						</>
+					) : (
+						<>
+							<form
+								action={async () => {
+									"use server";
+									await signIn("google");
+								}}
+							>
+								<button type="submit">Login</button>
+							</form>
+						</>
+					)}
+				</div>
 			</nav>
 		</div>
 	);
