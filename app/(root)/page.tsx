0630716024af83223284eaf3151a741748d06f65
karanssu <s0377009salemstate@gmail.com>
2024-12-16 23:35:12 -0500

    SearchForm added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import SearchForm from "../components/SearchForm";
+
+export default async function Home({
+	searchParams,
+}: {
+	searchParams: Promise<{ query?: string }>;
+}) {
+	const query = (await searchParams).query || "";
+
 	return (
 		<>
 			<section className="pink_container">
@@ -8,6 +16,8 @@ export default function Home() {
 				<p className="sub-heading !max-w-3xl">
 					Submit Ideas, Vote on Pitches, Get Noticed in Virtual Competetions.
 				</p>
+
+				<SearchForm query={query} />
 			</section>
 		</>
 	);
