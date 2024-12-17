f1e41e0a73da04951fec2bb933265abb99953ac2
karanssu <s0377009salemstate@gmail.com>
2024-12-16 23:42:25 -0500

    X and Search Icons added

--- app/components/SearchFormReset.tsx
+++ app/components/SearchFormReset.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { X } from "lucide-react";
 import Link from "next/link";
 
 const SearchFormReset = () => {
@@ -14,7 +15,7 @@ const SearchFormReset = () => {
 	return (
 		<button type="reset" onClick={reset}>
 			<Link href="/" className="search-btn text-white">
-				X
+				<X />
 			</Link>
 		</button>
 	);
