8808e55813ccaa01ecf997dcba03dbdb4cd95f0c
karanssu <s0377009salemstate@gmail.com>
2024-12-16 21:33:16 -0500

    Titles added in the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
 	return (
 		<>
-			<h1 className="heading">Home</h1>
+			<section className="pink_container">
+				<h1 className="heading">
+					Pitch Your Startup, <br /> Connect With Entrepreneurs
+				</h1>
+				<p className="sub-heading !max-w-3xl">
+					Submit Ideas, Vote on Pitches, Get Noticed in Virtual Competetions.
+				</p>
+			</section>
 		</>
 	);
 }
